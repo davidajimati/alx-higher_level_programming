b4b2ba944304cd012b639654ef058e05748d9df4
Blockeng <davidflinty@gmail.com>
2022-12-01 18:21:04 +0100

    edits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        print("0 arguments.")
+        print("{:d} arguments." .format(0))
     elif len(argv) == 2:
         print("1 argument:")
         print("{:d}" .format(1), argv[1])
