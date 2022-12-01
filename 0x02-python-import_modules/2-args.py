889a564f21f4a2f9c469b54d011cd81d289e8837
Blockeng <davidflinty@gmail.com>
2022-12-01 18:18:46 +0100

    typo corrected

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,12 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        print("0 Arguments.")
+        print("0 arguments.")
     elif len(argv) == 2:
-        print("1 Argument:")
+        print("1 argument:")
         print("1: ", argv[1])
     else:
-        print(len(argv) - 1, "Arguments: ")
+        print(len(argv) - 1, "arguments:")
         for i in range(len(argv)):
             if i == 0:
                 continue
