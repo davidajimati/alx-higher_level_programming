bdbff55cc03547d7a283029e579a9b7443ce5634
Blockeng <davidflinty@gmail.com>
2022-12-01 18:15:49 +0100

    edits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
         print("0 Arguments.")
     elif len(argv) == 2:
         print("1 Argument:")
-        print("1:", argv[1])
+        print("1: ", argv[1])
     else:
-        print(len(argv) - 1, "Arguments:")
+        print(len(argv) - 1, "Arguments: ")
         for i in range(len(argv)):
             if i == 0:
                 continue
