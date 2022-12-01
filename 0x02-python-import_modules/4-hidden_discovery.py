4f577dea5806743c80ccd480c8404ed51336369c
Blockeng <davidflinty@gmail.com>
2022-12-01 20:08:51 +0100

    if __name__ added

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-import hidden_4
+if __name__ == "__main__":
+    import sys
+    import hidden_4
 
-for i in dir(hidden_4):
-    if i[:2] != "__":
-        print(i)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
