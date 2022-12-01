ae1ea22f5f10391f874b120a00888724eda7d6c4
Blockeng <davidflinty@gmail.com>
2022-12-01 18:13:51 +0100

    if __name added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 import sys
 from sys import argv
 
-if len(argv) == 1:
-    print("0 Arguments.")
-elif len(argv) == 2:
-    print("1 Argument:")
-    print("1:", argv[1])
-else:
-    print(len(argv) - 1, "Arguments:")
-    for i in range(len(argv)):
-        if i == 0:
-            continue
-        print("{:d}:" .format(i), argv[i])
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 Arguments.")
+    elif len(argv) == 2:
+        print("1 Argument:")
+        print("1:", argv[1])
+    else:
+        print(len(argv) - 1, "Arguments:")
+        for i in range(len(argv)):
+            if i == 0:
+                continue
+            print("{:d}:" .format(i), argv[i])
