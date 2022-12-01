63443859e5a62332630f062a4d45e80fa7bb19ce
Blockeng <davidflinty@gmail.com>
2022-12-01 18:10:57 +0100

    executable permission included

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-
 if len(sys.argv) == 1:
     print("0 Arguments.")
 elif len(sys.argv) == 2:
     print("1 Argument:")
     print("1:", sys.argv[1])
 else:
-    print(len(sys.argv), "Arguments:")
+    print(len(sys.argv) - 1, "Arguments:")
     for i in range(len(sys.argv)):
+        if i == 0:
+            continue
         print("{:d}:" .format(i), sys.argv[i])
