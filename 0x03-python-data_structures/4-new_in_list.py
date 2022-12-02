5c1a861b4ab846d5670b0d2b3950943cb1300d8b
Blockeng <davidflinty@gmail.com>
2022-12-02 11:24:22 +0100

    create a copy of a list and modify it

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
+
+    new = my_list.copy()
+    new[idx] = element
+    return new
