41bae8a754b8230b8e368ca26d8c8f4c519f1a68
Blockeng <davidflinty@gmail.com>
2022-12-04 18:56:15 +0100

    corrected

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,10 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0 and len(tuple_b) == 0:
         return None
 
-    l1 = list(tuple_a)
-    l2 = list(tuple_b)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
 
-    l1 += [0, 0]
-    l2 += [0, 0]
-
-    return tuple(l1[0] + l2[0], l1[1] + l2[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
