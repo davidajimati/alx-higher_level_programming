e49a0161a006668405894e5c4cd31aa20ddbf5c4
Blockeng <davidflinty@gmail.com>
2022-12-04 19:09:24 +0100

    empty list result included

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # returns the larget number in a list
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
     ans = my_list[0]
 
     for i in my_list:
