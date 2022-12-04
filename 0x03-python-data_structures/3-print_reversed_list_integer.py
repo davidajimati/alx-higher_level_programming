5b5a546fb4f95e4f848e97480a3fbb5d0e1e5a2e
Blockeng <davidflinty@gmail.com>
2022-12-04 15:40:59 +0100

    updated

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    tot = len(my_list)
+    tot = len(my_list) - 1
     while tot >= 0:
-        tot -= 1
         print("{:d}" .format(my_list[tot]))
+        tot -= 1
