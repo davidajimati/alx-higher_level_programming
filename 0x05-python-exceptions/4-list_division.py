0faea2aac827cb088f32ca115decc378a44f7cdf
davidajimati <ajimatidavid@gmail.com>
2022-12-19 18:13:10 +0100

    more compact, try and except instead of loops and conditionals

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    l1 = my_list_1
-    l2 = my_list_2
-    try:
-        for j in l2:
-            if j == 0:
+        new = []
+        # tot = max(my_list_1, my_list_2)
+        count = 0
+        answer = 0
+
+        for i in range(len(my_list_1)):
+            try:
+                answer = my_list_1[i] / my_list_2[i]
+            except TypeError:
+                answer = 0
+                print("wrong type")
+            except ZeroDivisionError:
+                answer = 0
                 print("division by 0")
+            except IndexError:
+                answer = 0
+                print("out of range")
+            finally:
+                new.append(answer)
 
-        if not (isinstance(l1, (int, float)) and isinstance(l2, (int, float))):
-            print("wrong type")
-
-        if list_length > len(l1) or list_length > len(l2):
-            raise IndexError
-
-    except IndexError:
-        print("out of range")
-
-    finally:
-        aux = max(l1, l2)
-        new_list = []
-        for i in range(len(aux)):
-            if isinstance(aux[i], (int, float)) and l2[i]:
-                if isinstance(l2[i], (int, float)):
-                    new_list.append(aux[i] / l2[i])
-                else:
-                    new_list.append(0)
-            else:
-                new_list.append(0)
-        return (new_list)
+        return (new)
