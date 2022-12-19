cfa4f2e66c2fd7243acf6f2511f7bed729072628
davidajimati <ajimatidavid@gmail.com>
2022-12-19 18:14:12 +0100

    indent corrected

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-        new = []
-        # tot = max(my_list_1, my_list_2)
-        count = 0
-        answer = 0
+    new = []
+    # tot = max(my_list_1, my_list_2)
+    count = 0
+    answer = 0
 
-        for i in range(len(my_list_1)):
-            try:
-                answer = my_list_1[i] / my_list_2[i]
-            except TypeError:
-                answer = 0
-                print("wrong type")
-            except ZeroDivisionError:
-                answer = 0
-                print("division by 0")
-            except IndexError:
-                answer = 0
-                print("out of range")
-            finally:
-                new.append(answer)
+    for i in range(len(my_list_1)):
+        try:
+            answer = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            answer = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            answer = 0
+            print("division by 0")
+        except IndexError:
+            answer = 0
+            print("out of range")
+        finally:
+            new.append(answer)
 
-        return (new)
+    return (new)
