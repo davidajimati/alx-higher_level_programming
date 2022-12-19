74c26ee15577d267b643e5f4145e0f463fa41a70
davidajimati <ajimatidavid@gmail.com>
2022-12-19 18:57:31 +0100

    exception as E removed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,11 @@ import sys
 
 
 def safe_print_integer_err(value):
+    from sys import stderr
     try:
         print("{:d}" .format(value))
         return (True)
 
-    except (ValueError, TypeError) as err:
+    except (ValueError, TypeError):
         print("Exception: {}" .format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
