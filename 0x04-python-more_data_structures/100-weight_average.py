7e16250e2d316266dcc13d8413c1409c205e916e
davidajimati <ajimatidavid@gmail.com>
2022-12-20 10:45:32 +0100

    weighted average function

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,6 +4,15 @@ def weight_average(my_list=[]):
     #if list is empty return 0
     if not my_list or len(my_list) == 0:
         return (0)
-    #declare a variable to hold the final answer
-    #declare a variable to hold the result of the division of each cell
+
+    #declare variable to hold answer and the division of mul cell
+    answer = 0
+    mul = 0
+    den = 0
+
     # loop through the list and effect the calculations
+    for i in my_list:
+        mul += i[0] * i[1]
+        den += i[1]
+    answer = mul / den
+    return (answer)
