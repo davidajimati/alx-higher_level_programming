4d988f7a2b9864fc0825c90f58ef145ad4895828
davidajimati <ajimatidavid@gmail.com>
2022-12-21 20:38:15 +0100

    attribute placed after conditional statement

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,15 +8,10 @@ class Square:
     '''
 
     def __init__(self, size=0):
-        try:
-            self.__size = size
-            if type(size) != int:
-                raise TypeError
+        if type(size) != int:
+            raise TypeError("size must be an integer")
 
-            if size < 0:
-                raise ValueError
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        self.__size = size
