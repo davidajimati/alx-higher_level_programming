9cc0f1e0572ac454a8486dd31732b5663ff07b84
davidajimati <ajimatidavid@gmail.com>
2022-12-21 19:40:01 +0100

    correct now

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    pass
-    """Example function with types documented in the docstring.
-
-    `PEP 484`_ type annotations are supported. If attribute, parameter, and
-    return types are annotated according to `PEP 484`_, they do not need to be
-    included in the docstring:
-
-    """
+    def __init__(self):
+        pass
