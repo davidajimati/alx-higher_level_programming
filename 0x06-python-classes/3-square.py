ee29e5be46b96e197ecef15ee19a90db685831d5
davidajimati <ajimatidavid@gmail.com>
2022-12-21 20:42:05 +0100

    area os square 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Declaring a square method.
+'''
+
+
+class Square:
+    ''' Declares class string.
+    '''
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
