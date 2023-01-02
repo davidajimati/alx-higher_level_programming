cabf78b6a9cf0663edd1944861f406b6795d5bff
davidajimati <ajimatidavid@gmail.com>
2023-01-02 16:12:43 +0100

    exceptions added

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,5 +81,9 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         cls.width, cls.height = size, size
         return cls(cls.width, cls.height)
