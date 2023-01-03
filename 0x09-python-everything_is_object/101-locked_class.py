8130c80a77d6c6e235db48348c7149fde8602350
davidajimati <ajimatidavid@gmail.com>
2023-01-03 13:54:29 +0100

    101 locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,10 +4,16 @@
 Locked class with no object or class attribute
 '''
 
+
 class LockedClass:
+    '''prevents the user from dynamically creating new instance
+attributes, except if the new instance attribute is
+called first_name.
     '''
-    prevents the user from dynamically creating new instance 
-    attributes, except if the new instance attribute is 
-    called first_name.
-    '''
-    pass
+
+    def __setattr__(self, attribute, value):
+        if attribute == 'first_name':
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError\
+        ("'LockedClass' object has no attribute 'last_name'")
