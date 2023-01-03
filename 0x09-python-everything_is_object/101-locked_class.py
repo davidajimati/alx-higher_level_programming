d532831b71801d0492ff5e24d9cb7a8ebcc08ad9
davidajimati <ajimatidavid@gmail.com>
2023-01-03 14:02:22 +0100

    error message formated

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -12,8 +12,8 @@ called first_name.
     '''
 
     def __setattr__(self, attribute, value):
-        if attribute == 'first_name':
+        if attribute == "first_name":
             self.__dict__[attribute] = value
         else:
             raise AttributeError(
-                "'LockedClass' object has no attribute 'last_name'")
+                "'LockedClass' object has no attribute '{}'" .format(attribute))
