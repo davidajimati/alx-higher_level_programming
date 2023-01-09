b141cad947b20105172d22318c1eddc77c651aae
davidajimati <ajimatidavid@gmail.com>
2023-01-09 14:01:35 +0100

    check isinstance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' FUnction to check if a child class
+    is an instance of a parent class
+'''
+
+
+def is_same_class(obj, a_class):
+    ''' IS SAME CLASS '''
+    return isinstance(obj, a_class)
