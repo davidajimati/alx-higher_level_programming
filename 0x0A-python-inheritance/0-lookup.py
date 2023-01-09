9496de47ff74ce156eaef06196cba90bca0d766b
davidajimati <ajimatidavid@gmail.com>
2023-01-09 12:03:09 +0100

    lookup function created

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+Function to return the list of special attributes applicable to an object
+'''
+
+
+def lookup(obj):
+    '''
+    Accepts an object and checks the type. If not None, then returns the dir(ob)
+    '''
+    if obj == None:
+        return (None)
+    return (dir(obj))
