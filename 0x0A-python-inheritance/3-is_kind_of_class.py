12eb79bd4127ea7e0bb53fe57efd537bcbff9a02
davidajimati <ajimatidavid@gmail.com>
2023-01-09 14:12:16 +0100

    is kind of class function

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr.bin/python3
+
+'''THis module shows if a class is a subclass of another'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    function declaration proper
+    '''
+    return isinstance(obj, a_class)
