1e57db1052edd018ca3579e93080a62415d19ded
davidajimati <ajimatidavid@gmail.com>
2023-01-09 15:15:25 +0100

    #fix: 'is not" change to '!='

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Method for validate if a num is integer"""
 
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
