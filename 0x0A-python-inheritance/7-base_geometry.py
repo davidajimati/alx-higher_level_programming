a488ac36ddfcef95ad939c4487bc93d3546941ae
davidajimati <ajimatidavid@gmail.com>
2023-01-09 15:12:49 +0100

    fix: method tweak

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,9 +12,9 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ validates if value is an integer"""
+        """Method for validate if a num is integer"""
 
         if type(value) is not int:
-            raise TypeError("{} must be an integer" .format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0" .format(name))
+            raise ValueError("{} must be greater than 0".format(name))
