601e4106745eca95e6db338933e00b65a23c18b4
davidajimati <ajimatidavid@gmail.com>
2023-01-09 14:29:58 +0100

    6-base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Declaration of an empty class
+'''
+
+
+class BaseGeometry:
+    """ Class declared and passed """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+push
