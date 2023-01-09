b487ce42aede52075f5db07e09da950efeb6dd5f
davidajimati <ajimatidavid@gmail.com>
2023-01-09 16:26:28 +0100

    #fix: documentations included

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,11 @@
 
 
 class MyInt(int):
+    ''' Class declaration '''
     def __eq__(self, object) -> bool:
+        ''' Remodifying the function of "==" '''
         return not int.__eq__(self, object)
 
     def __ne__(self, object) -> bool:
+        ''' Remodifying the function of "!=" '''
         return not int.__ne__(self, object)
