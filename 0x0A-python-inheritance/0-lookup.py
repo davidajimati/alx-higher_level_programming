65a4cd88295a38a6142abf37b95e35474ae365fc
davidajimati <ajimatidavid@gmail.com>
2023-01-09 12:04:47 +0100

    pycodestyle implemented

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,8 +7,9 @@ Function to return the list of special attributes applicable to an object
 
 def lookup(obj):
     '''
-    Accepts an object and checks the type. If not None, then returns the dir(ob)
+    Accepts an object and checks the type.
+    If not None, then returns the dir(ob)
     '''
-    if obj == None:
+    if obj is None:
         return (None)
     return (dir(obj))
