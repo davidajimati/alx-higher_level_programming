553645926552d821b235c94c54ff4a6c24cca443
davidajimati <ajimatidavid@gmail.com>
2023-01-09 14:04:08 +0100

    if statement updated

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 
 
 def is_same_class(obj, a_class):
-    if obj is None or a_class is None:
+    if obj is None and a_class is None:
         return None
     ''' IS SAME CLASS '''
     return isinstance(obj, a_class)
