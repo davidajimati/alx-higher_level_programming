3681948a08aac011a029f7ed54bc4c706f833435
davidajimati <ajimatidavid@gmail.com>
2023-01-09 17:40:15 +0100

    feat: created add attribute function

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+*function that adds a new attribute to an object if it’s possible
+*Raise a TypeError exception, with the message
+can't add new attribute if the object can’t have new attribute
+*You are not allowed to use try/except
+*You are not allowed to import any module
+'''
+
+
+def add_attribute(object, item, value):
+    if object:
+        setattr(object, item, value)
+        return
+    raise TypeError("can't add new attribute")
