1a3b3eb74834f733e95d857cdc79e4e594596a0f
davidajimati <ajimatidavid@gmail.com>
2023-01-09 16:11:35 +0100

    #fix: documentation added

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,12 +9,17 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    ''' Class declared'''
+
     def __init__(self, size):
+        ''' Constructor defined '''
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        ''' Area method implemented '''
         return self.__size * self.__size
 
     def __str__(self):
+        ''' Print function modified '''
         return ("[Rectangle] {}/{}" .format(self.__size, self.__size))
