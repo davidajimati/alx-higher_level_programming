f4b42f10e885cd161df286bd9221a35f41a2ee3d
davidajimati <ajimatidavid@gmail.com>
2023-01-09 13:56:07 +0100

    child class 'pass'ed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,7 @@ Child class inherits from predefined Parent class 'list'
 
 class MyList(list):
     """ child class 'My list' """
+    pass
 
     def print_sorted(self):
         print(sorted(list(self)))
