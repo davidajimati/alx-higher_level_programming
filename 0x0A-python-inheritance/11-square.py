a592b6b18ab4805ec134c359db814da609b6dc6d
davidajimati <ajimatidavid@gmail.com>
2023-01-09 16:13:57 +0100

    feat: __str__ returns 'square' instead of 'Rectangle'

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+Creation of class 'Square' to inherit
+Rectangle parent class
+'''
+Rectangle = __import__('9-rectangle').Rectangle
+''' Imported Rectangle module '''
+
+
+class Square(Rectangle):
+    ''' Class declared'''
+
+    def __init__(self, size):
+        ''' Constructor defined '''
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        ''' Area method implemented '''
+        return self.__size * self.__size
+
+    def __str__(self):
+        ''' Print function modified '''
+        return ("[Square] {}/{}" .format(self.__size, self.__size))
