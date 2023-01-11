b09c14511d5fa821eb8a2a3226d84b17db270c58
davidajimati <ajimatidavid@gmail.com>
2023-01-11 10:51:06 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+This module opens a file in UTF8 and
+prints its content to stdout
+'''
+def read_file(filename=""):
+    ''' Reads filename as file '''
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This folder contains project under INPUT AND OUTPUT FILE OPERATIONS in python.
