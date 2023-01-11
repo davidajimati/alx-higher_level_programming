17d0978c54f9a3aac50e82e84cbee908e2bec4b6
davidajimati <ajimatidavid@gmail.com>
2023-01-11 11:14:47 +0100

    decumentation addd=ed

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,7 @@ Module to append input to an existing file
 
 
 def append_write(filename="", text=""):
+    ''' Appends to a new file or create  a new one'''
     with open(filename, 'a', encoding="utf-8") as file:
         file.write(text)
         return (len(text))
