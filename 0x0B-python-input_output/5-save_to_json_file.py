35ae44cab2b2768f8f396daaa188308dfb300786
davidajimati <ajimatidavid@gmail.com>
2023-01-11 12:26:50 +0100

    #FIx: fixed printing

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,5 +10,9 @@ def save_to_json_file(my_obj, filename):
     '''
     Saves a python object into a text file in JSON representation
     '''
-    with open(filename, 'rw') as f:
-        json.dumps(my_obj, f)
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
+
+    with open(filename, 'r') as file:
+        read_v = json.load(file)
+        print(read_v)
