5ffbae6c81b5500e562aa11377dafdc2a1b40625
davidajimati <ajimatidavid@gmail.com>
2023-01-13 10:22:49 +0100

    #FIX: Key value pairs renamed appropriately

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,8 +10,8 @@ class Student:
     ''' Class Student '''
 
     def __init__(self, first_name, last_name, age):
-        self.first = first_name
-        self.last = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
