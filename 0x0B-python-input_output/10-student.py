fc5b365e4217d38de5dc377e76ada7d76cdd099f
davidajimati <ajimatidavid@gmail.com>
2023-01-13 10:45:45 +0100

    #FEAT: adds only specified attribute to dict

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+This module builds on the module on 9-student.py
+returns only the key pairs if attrs is a list of strings
+else returns all the dictionary values
+'''
+
+
+class Student:
+    ''' Class Student '''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Public method to return the dictionary of each instance"""
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            ret_dict = {}
+            for i in attrs:
+                ret_dict += self.__dict__[i]
+            return ret_dict
