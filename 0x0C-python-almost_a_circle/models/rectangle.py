0589fe1f05c4d57bcc5f4ef0e4095fa20cd01c06
davidajimati <ajimatidavid@gmail.com>
2023-01-17 10:03:21 +0100

    #FEAT: super class called

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' Constructor for class '''
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
