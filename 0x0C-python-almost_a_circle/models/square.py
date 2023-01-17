f3776d6307f02b33da731c1927251f38ef2b1b3e
davidajimati <ajimatidavid@gmail.com>
2023-01-17 15:53:21 +0100

    #FEAT: square clss created

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+Module contains a subclass that inherits from Rectangle
+'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Constructor'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self) -> str:
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))
