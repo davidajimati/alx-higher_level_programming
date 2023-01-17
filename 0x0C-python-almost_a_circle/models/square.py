7804f273a79b9405b674862d468f9bf230332d5b
davidajimati <ajimatidavid@gmail.com>
2023-01-17 16:01:44 +0100

    #FEAT: square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,10 +18,33 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        '''getter'''
-        return (self.size)
+        '''Size of this square.'''
+        return self.width
 
     @size.setter
     def size(self, value):
-        ''' setter '''
-        self.size = value
+        self.width = value
+        self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
