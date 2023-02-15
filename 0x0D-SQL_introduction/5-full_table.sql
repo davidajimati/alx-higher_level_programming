b7de88b28d5e44576c76da73990d29688434ccd3
davidajimati <ajimatidavid@gmail.com>
2023-02-15 20:09:26 +0100

    edits

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server.
-SELECT first_table FROM hbtn_0c_0;
+SELECT first_table FROM hbtn_0c_0 WHERE table_name = 'first_table';
