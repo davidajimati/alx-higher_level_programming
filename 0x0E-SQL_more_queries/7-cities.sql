aa3d926dd9f1427cd585cbad9eff4bdd29ead40b
davidajimati <ajimatidavid@gmail.com>
2023-03-15 17:24:04 +0100

    comma added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL,
-    'state_id' INT NOT NULL FOREIGN KEY (states_id) REFERENCES states(id),
+    state_id INT NOT NULL, FOREIGN KEY (states_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
