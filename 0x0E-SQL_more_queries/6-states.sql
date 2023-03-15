bfcd0835177c032e781d4638b22b9d7b525f709a
davidajimati <ajimatidavid@gmail.com>
2023-03-15 17:13:06 +0100

    creates a database, table with a foreign key that points to another table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL ,
+    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL,
     name VARCHAR(256) NOT NULL
 );
