3e2702a3f37f618550aba8c44ac9c197c046520d
davidajimati <ajimatidavid@gmail.com>
2023-03-15 16:34:26 +0100

    script to create new user and grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script that creates the MySQL server user user_0d_1.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
