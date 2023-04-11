c4271a53eea7e73d7c4f74f8305e8904135b2a2e
davidajimati <ajimatidavid@gmail.com>
2023-04-11 12:23:43 +0100

    concat and create sentences

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args.length < 2) {
+  console.log('undefined is undefined');
+} else if (!args[3]) {
+  console.log(args[2] + ' is ' + 'undefined');
+} else {
+  console.log(args[2] + ' is ' + args[3]);
+}
