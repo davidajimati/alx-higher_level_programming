f9cf35914081a647a7bffe4d399274b171e1f704
davidajimati <ajimatidavid@gmail.com>
2023-04-12 13:51:35 +0100

    hardcode undefined value removed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 | h <= 0) {
-      this.h = undefined;
-      this.w = undefined;
+      return;
     } else {
       this.width = w;
       this.height = h;
