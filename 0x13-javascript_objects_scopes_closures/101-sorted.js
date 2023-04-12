3fea6c2d0be074284304044ef79d73f057d781be
davidajimati <ajimatidavid@gmail.com>
2023-04-12 14:24:36 +0100

    in process

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
+
+let newDict = {};
+for (let key in dict) {
+  if (!(dict[key]) in newDict) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);
