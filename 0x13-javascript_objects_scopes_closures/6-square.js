51b01fcaa22530ab4b2ba75c2369df870c49d855
davidajimati <ajimatidavid@gmail.com>
2023-04-12 13:31:39 +0100

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,17 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
+  constructor(size) {
     super();
     this.size = size;
     this.height = size;
     this.width = size;
   }
 
-  charPrint (c) {
-    const symbol = c || 'X';
+  charPrint(c) {
+    if (c != undefined) {
+      const symbol = c || 'X';
+    }
     const { size } = this;
 
     for (let i = 0; i < size; i++) {
