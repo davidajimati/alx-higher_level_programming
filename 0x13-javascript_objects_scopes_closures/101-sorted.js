f2ec91ef6a8427990cc22dd9423ec87e94ed9b2d
davidajimati <ajimatidavid@gmail.com>
2023-04-12 14:27:37 +0100

    new keys created with corresponding lists

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@ const dict = require('./101-data.js').dict;
 
 let newDict = {};
 for (let key in dict) {
-  if (!(dict[key]) in newDict) {
+  if (!(dict[key] in newDict)) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
