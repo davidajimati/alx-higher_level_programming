235936f1c9e86cec42526d36ea74a7b14d47f646
davidajimati <ajimatidavid@gmail.com>
2023-04-12 13:10:55 +0100

    style standards obeyed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-let newArr = []
-if (list.length == 0)
-  console.log(newArr);
+let newArr = [];
+if (list.length === 0) { console.log(newArr); }
 
 newArr = list.map((i) => i * list.indexOf(i));
 console.log(list);
