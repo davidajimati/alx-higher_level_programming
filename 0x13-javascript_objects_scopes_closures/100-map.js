9173e01dbc85f1b09cdbc1a7076215fa24bc6af1
davidajimati <ajimatidavid@gmail.com>
2023-04-12 13:12:13 +0100

    error tracing

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 let newArr = [];
-if (list.length === 0) { console.log(newArr); }
-
-newArr = list.map((i) => i * list.indexOf(i));
+if (list.length === 0) { console.log(newArr); } else {
+  newArr = list.map((i) => i * list.indexOf(i));
+}
 console.log(list);
 console.log(newArr);
