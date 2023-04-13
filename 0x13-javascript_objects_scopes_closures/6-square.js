8510a7adf98e0a3eba5b02a73c6bd445ca7c8d69
davidajimati <ajimatidavid@gmail.com>
2023-04-13 08:45:35 +0100

    semistandard codestyle implemented

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 module.exports = class Square extends require('./5-square') {
-
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
