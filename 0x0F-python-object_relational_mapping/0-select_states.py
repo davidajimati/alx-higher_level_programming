a8746ffb17015f7b01325dd9685fc9cf241ade06
davidajimati <ajimatidavid@gmail.com>
2023-04-18 15:28:07 +0100

    if __name__=="__main__"; added to module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,13 +3,15 @@
 This module contains a script that script that lists
 all states from the database hbtn_0e_0_usa
 """
-import MySQLdb
-from sys import argv
 
-db = MySQLdb.connect(host='localhost', port=3306,
-                     user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
 
-cur = db.cursor()
-res = cur.execute("SELECT * FROM  states ORDER BY states.id ASC;")
-for item in res:
-    print(item)
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    res = cur.execute("SELECT * FROM  states ORDER BY states.id ASC;")
+    for item in res:
+        print(item)
