6eb0126033cf7e08161e497f52d2b3888c2cdb59
davidajimati <ajimatidavid@gmail.com>
2023-04-18 16:22:07 +0100

    BINARY keyword added to querry

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa"""
+"""
+This module lists all states
+    with a name starting with N (upper N) from
+    the database hbtn_0e_0_usa
+"""
 
 if __name__ == '__main__':
 
     import MySQLdb
-    import sys
+    from sys import argv
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("""SELECT * FROM states WHERE name BINARY
+                LIKE 'N%' ORDER BY states.id ASC""")
+    output = cur.fetchall()
+    for state in output:
+        print(state)
