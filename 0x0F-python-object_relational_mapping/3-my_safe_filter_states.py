d9d33c80202217e186b8f589bc80ef6fc9c4da4a
davidajimati <ajimatidavid@gmail.com>
2023-04-18 17:30:13 +0100

    injection guard -> modifications

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     search_query = (argv[4] + '%')
-    sql_query = """SELECT * FROM states WHERE name LIKE '{}' ORDER
-                     BY states.id ASC""".format(search_query)
+    sql_query = """SELECT * FROM states WHERE name LIKE %s ORDER
+                     BY states.id ASC""".format(search_query, )
     cur = db.cursor()
     cur.execute(sql_query)
     items = cur.fetchall()
