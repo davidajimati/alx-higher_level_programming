ad69afcf7aa7e1e6bb8e8ad06d78f8e6e457da77
davidajimati <ajimatidavid@gmail.com>
2023-04-18 15:31:26 +0100

    res called on same line with cur.execute()

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM  states ORDER BY states.id ASC;")
-    res = cur.fetchall()
+    res = cur.execute("SELECT * FROM  states ORDER BY states.id ASC;")
     for item in res:
         print(item)
