a28dce338c5ba3dc7f490c1fc2904e1a6c7392f2
davidajimati <ajimatidavid@gmail.com>
2023-04-18 17:14:39 +0100

    safe filter -> injextion guard

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where
+"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    search_query = (argv[4] + '%')
+    sql_query = """SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER
+                     BY states.id ASC""".format(search_query)
+    cur = db.cursor()
+    cur.execute(sql_query)
+    items = cur.fetchall()
+
+    for item in items:
+        print(item)
