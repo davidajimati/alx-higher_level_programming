6068c9743d46aec076e58ae4bdcfc23333000aec
davidajimati <ajimatidavid@gmail.com>
2023-04-18 15:23:40 +0100

    select sattes script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-from sqlalchemy import engine, create_engine, Column, Integer, CHAR,  String
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host='localhost', port=3306,
+                     user=argv[1], passwd=argv[2], db=argv[3])
+
+cur = db.cursor()
+res = cur.execute("SELECT * FROM  states ORDER BY states.id ASC;")
+for item in res:
+    print(item)
