5856c29843353ff4f242d399266640727551bd32
davidajimati <ajimatidavid@gmail.com>
2023-04-18 15:42:30 +0100

    filter states by initials -> script

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains a script that lists all states
+    with a name starting with N (upper N) from
+    the database hbtn_0e_0_usa
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    output = cur.fetchall()
+    for state in output:
+        if state(0) == 'N':
+            print(state)
