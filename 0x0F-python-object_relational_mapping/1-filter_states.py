9c6eabe86dc241a0986a75d330975e4bd320b61f
davidajimati <ajimatidavid@gmail.com>
2023-04-18 16:13:15 +0100

    pycodestyle guide applied

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
                          user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE 'N%' ORDER BY states.id ASC;""")
     output = cur.fetchall()
     for state in output:
         print(state)
