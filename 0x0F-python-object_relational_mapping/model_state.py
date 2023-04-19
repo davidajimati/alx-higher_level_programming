083a05e5f9f138588d9ac7438e5249734f9043cb
davidajimati <ajimatidavid@gmail.com>
2023-04-19 10:45:05 +0100

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,8 @@ State and an instance Base = declarative_base()
 """
 
 from enum import unique
-from sqlalchemy import Column, Integer, CHAR, String
+from sys import argv
+from sqlalchemy import Column, Integer, CHAR, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base
@@ -18,3 +19,8 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True,
                 primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+engine = create_engine(
+    'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+Base.metadata.create_all(engine)
