fdcb2d5b6d16fec0dd701a402ef31082a7831b10
davidajimati <ajimatidavid@gmail.com>
2023-04-19 11:03:24 +0100

    testing

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,12 +5,9 @@ State and an instance Base = declarative_base()
 """
 
 from enum import unique
-from sqlalchemy import Column, Integer, CHAR, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-'''
-import of necessary modules
-'''
 Base = declarative_base
 
 
@@ -20,6 +17,6 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, unique=True,
-                primary_key=True, nullable=False)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
