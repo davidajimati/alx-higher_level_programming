6c2ea6e5f9d8d685be93d6bef8d6488e0a08841a
davidajimati <ajimatidavid@gmail.com>
2023-04-19 10:58:11 +0100

    updated

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
-'''
-This module implements the solution
-importing from the model state
+'''implements the solution importing from the model state
 '''
 from model_state import State, Base
 from sys import argv
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    '''
-    Below: specifies the url and the command to create the engine
-    '''
     engine = create_engine(
-        'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
