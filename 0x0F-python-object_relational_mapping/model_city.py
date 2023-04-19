e034b87d43166521eda654856f62c135e6a68d35
davidajimati <ajimatidavid@gmail.com>
2023-04-19 15:27:13 +0100

    model city created

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""python file that contains the class definition of
+a city and an instance Base = declarative_base()"""
+
+from enum import unique
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class city(Base):
+    """Class city"""
+
+    __tablename__ = 'cities'
+    id = Column("id", Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
+
+    name = Column("name",String(128), nullable=False)
+    state_id = Column("state_id",Integer, ForeignKey('states.id'), nullable=False)
