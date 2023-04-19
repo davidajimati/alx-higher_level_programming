fcf6f680ce1038f746cc646c3b21ad64611446b1
davidajimati <ajimatidavid@gmail.com>
2023-04-19 14:30:29 +0100

    script to add a new state entry to table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that adds the State object “Louisiana” to the
+    database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}",
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    new_state = State(id=6, name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
