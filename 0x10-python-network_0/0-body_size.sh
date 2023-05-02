21590541a4c2226c8a7efb0d8bb98e6cefb28241
davidajimati <ajimatidavid@gmail.com>
2023-05-02 09:01:54 +0100

    tail -n1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,5 @@
 #  takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
 response=$(curl -s -w "\n%{size_download}" "$1")
-size=$(echo "$response" | tail -n2)
+size=$(echo "$response" | tail -n1)
 echo "$size"
