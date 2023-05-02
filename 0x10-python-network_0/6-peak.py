927e42e5ed0ed50235de471acab25164af2bddb3
davidajimati <ajimatidavid@gmail.com>
2023-05-02 09:46:32 +0100

    pycodestyle implemented

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 This function finds the peak of a  give set of numbers
 """
 
+
 def find_peak(list_of_integers):
     '''
     Function declaration
@@ -11,4 +12,4 @@ def find_peak(list_of_integers):
     for i in list_of_integers:
         if i > x:
             x = i
-    return(x)
+    return (x)
