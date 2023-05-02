4de17faa04451802b9aae4fdeb13848b18b17933
davidajimati <ajimatidavid@gmail.com>
2023-05-02 09:44:32 +0100

    function updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,7 +8,7 @@ def find_peak(list_of_integers):
     Function declaration
     '''
     x = 0
-    for i in range(list_of_integers):
-        if list_of_integers[i] > x:
-            x = list_of_integers[i]
+    for i in list_of_integers:
+        if i > x:
+            x = i
     return(x)
