d624780ba937e725c65fc652a165c9f7cefc42dc
davidajimati <ajimatidavid@gmail.com>
2023-05-03 16:38:32 +0100

    4-hbtn_status.py module updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:$")
     print("\t- type: {}$".format(type(r.text)))
-    print("\t- content: {}$".format(r.content))
+    print("\t- content: {}$".format(r.text))
