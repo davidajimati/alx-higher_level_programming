9580e66e222348eae3780ee811d6f708b2821943
davidajimati <ajimatidavid@gmail.com>
2023-05-03 16:36:30 +0100

    4-hbtn_status.py created

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''
+This module fetches https://alx-intranet.hbtn.io/status
+    uses the package requests
+    No package imports other than requests
+'''
+
+import requests
+if __name__ == "__main__":
+
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:$")
+    print("\t- type: {}$".format(type(r.text)))
+    print("\t- content: {}$".format(r.content))
