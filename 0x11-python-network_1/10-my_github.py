ce8350c151e22be616db6bb139575289566f1437
davidajimati <ajimatidavid@gmail.com>
2023-05-04 21:21:14 +0100

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+This module takes your GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+'''
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+
+    if len(argv) > 2:
+        username = argv[1]
+        password = argv[2]
+
+        r = requests.get('https://api.github.com/user', auth=(username, password))
+
+        if r.status_code == 200:
+            print("{}".format(r.get("id")))
+        else:
+            print("None")
