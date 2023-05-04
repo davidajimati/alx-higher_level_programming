966a3cd0bd4c494e704f58d3d48d89d6fc5cdbbe
davidajimati <ajimatidavid@gmail.com>
2023-05-04 22:33:53 +0100

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-
 '''
 Interview Question - solution module
 '''
-
 import requests
 from sys import argv
+
 if __name__ == "__main__":
+
     repo = argv[1]
     owner = argv[2]
 
     url = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
     r = requests.get(url)
+    commits = r.json()
 
     if r.status_code == 200:
-        commits = r.json()
-
-        i = 0
-        while i < 10:
-            print("{}: {}" .format(commits[i].get('sha'), commits[i].get(
-                'commit').get('author').get('name')))
-            i += 1
+        try:
+            i = 10
+            while i > 0:
+                print("{}: {}" .format(commits[i].get('sha'), commits[i].get(
+                    'commit').get('author').get('name')))
+                i -= 1
+        except Exception:
+            pass
