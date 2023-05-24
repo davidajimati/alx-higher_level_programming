098d0cc28bc3bade78d594e7283be9cb2a4dc8cc
ajimatidavid <ajimatidavid@gmail.com>
2023-05-24 15:00:37 +0000

    100-starwars_characters.js added | 6-completed_tasks.js updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,14 +3,14 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
-    const record = {};
+    const completed = {};
     todos.forEach((todo) => {
-      if (todo.record && record[todo.userId] === undefined) {
-        record[todo.userId] = 1;
-      } else if (todo.record) {
-        record[todo.userId] += 1;
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-    console.log(record);
+    console.log(completed);
   }
 });
