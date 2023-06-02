f21b3c8dd148a2385a94d0e19455189045925532
davidajimati <ajimatidavid@gmail.com>
2023-06-02 17:14:31 +0100

    script 3-script.js

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+<script src='https://code.jquery.com/jquery-3.6.0.min.js' />;
+$(document).ready(function () {
+  $('div#red_header').click(function () {
+    $('header').addClass('red');
+  });
+});
